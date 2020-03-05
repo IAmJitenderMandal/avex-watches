bb19e58cb89abe168973e2413a4e6371d1ce481f
jitender mandal <jitender62012@gmail.com>
2020-03-05 20:30:32 +0530

    changes in data.json and filter options added dynamically according to the data we have in our json file

--- js/script.js
+++ js/script.js
@@ -49,11 +49,41 @@ function getProducts(callback) {
 getProducts(function (err, data) {
     if (err) {
         console.log(err);
-    } else if(data) {
+    } else if (data) {
         
+        // select all filter elements from document
+        var filters = document.querySelectorAll('#watchType , #gender, #price, #color, #dialType, #materialType, #shape, #movement, #collection');
+        // converting nodlist to an array
+        filters = Array.from(filters);
+        console.log(filters);
+
+        var filterVals = [];
+        for (var i = 0; i < filters.length; i++) {
+
+            // filter id
+            var filterID = filters[i].id;
+            
+            // puting each filter related value in an array
+            data.forEach(function (eachDataObj) {
+                filterVals.includes(eachDataObj[filterID]) ? null : filterVals.push(eachDataObj[filterID]);  
+            })
+
+            //now settiing dynamically all those option in filters according to the options we have in our data
+            filterVals.forEach(function(filterval) {
+                for (var i = 0; i < filters.length; i++) {
+                    if (filters[i].id === filterID) {
+                        filters[i].innerHTML += '<option value=' + filterval + '>' + filterval + '</option>';
+                    }
+                }
+            });
+            // making filtervals empty again so it would not have old data again
+            filterVals = [];
+        }
+      
         // show data on page
         var products = document.querySelectorAll('.each-product');
         products = Array.from(products);
+      
         products.forEach(function (product, index) {
             // removing loading status
             if (product.querySelector('.img-loading') && product.querySelector('.name-loading') && product.querySelector('.price-loading')) {
@@ -64,7 +94,7 @@ getProducts(function (err, data) {
 
             // inserting data to html elements
             product.querySelector('.product-img img').setAttribute('src', 'img/' + data[index].img);
-            product.querySelector('.product-name').textContent =  data[index].name;
+            product.querySelector('.product-name').textContent = data[index].name;
         });
     }
 });
